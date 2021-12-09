fb6e7b0a6a44c0d88f59ccd551e004b5fab75b44
Jack McLoughlin <jack.mcloughlin@alig2124.staff.aligent.consulting>
2021-12-10 09:26:59 +1030

    Created seperate functions for marking straight and diagonal lines, re doing the diagonal lines function

--- Advent/Day-5/part2.php
+++ Advent/Day-5/part2.php
@@ -22,9 +22,9 @@ function createFloor($input): array
 {
     $oceanFloor = array(array());
 
-    for($i = 0; $i < 1000; $i++)
+    for($i = 0; $i < 10; $i++)
     {
-        for($j = 0; $j < 1000; $j++)
+        for($j = 0; $j < 10; $j++)
         {
             $oceanFloor[$i][$j] = 0;
         }
@@ -44,20 +44,16 @@ echo "</pre>";
 print_r($setInput);
 echo "</pre>";*/
 
-function markFloor($oceanFloor, $input)
+function markFloorStraight($oceanFloor, $input)
 {
-
     $markedOceanFloor = $oceanFloor;
-
     //echo count($input); exit;
    for($i = 0; $i < count($input); $i++)
    {
         $x1 = $input[$i][0][0];
         $y1 = $input[$i][0][1];
-
         $x2 = $input[$i][1][0];
         $y2 = $input[$i][1][1];
-
        // from 0,9 and 5,9
        // need to generate 1,9 2,9 3,9 4,9
        if($y1 == $y2)
@@ -76,7 +72,6 @@ function markFloor($oceanFloor, $input)
            }
            $x2 = $input[$i][1][0];
        }
-
        // from 0,5 and 0,9
        // need to generate 0,6 0,7 0,8
        if($x1 == $x2)
@@ -95,78 +90,38 @@ function markFloor($oceanFloor, $input)
            }
            $y2 = $input[$i][1][1];
        }
-       // 5,5 -> 9,9 = 5,5 6,6 7,7 8,8 9,9
-       if($x1 == $y1 && $x2 == $y2)
-       {
-            while($x1 <= $x2)
-            {
-                $markedOceanFloor[intval($x1)][intval($y1)] += 1;
-                $x1++;
-                $y1++;
-            }
-           $x1 = $input[$i][0][0];
-           $y1 = $input[$i][0][1];
-       }
-
-       if($x1 == $y2 && $y1 == $x2)
-       {
-           // 9,7 -> 7,9 = 9,7 8,8 7,9
-           while ($y1 <= $y2)
-           {
-               $markedOceanFloor[intval($x1)][intval($y1)] += 1;
-               $x1--;
-               $y1++;
-           }
-           $x1 = $input[$i][0][0];
-           $y1 = $input[$i][0][1];
-           //7,9 -> 9,7 = 7,9 8,8 9,7
-           while ($x1 <= $x2)
-           {
-               $markedOceanFloor[intval($x1)][intval($y1)] += 1;
-               $x1++;
-               $y1--;
-           }
-           $x1 = $input[$i][0][0];
-           $y1 = $input[$i][0][1];
-       }
-       // 6,4 -> 2,0 = 6,4 5,3 4,2 3,1 2,0
-       if($x1 > $x2 && $y1 > $y2)
-       {
-           while($x1 >= $x2)
-           {
-               $markedOceanFloor[intval($x1)][intval($y1)] += 1;
-               $x1--;
-               $y1--;
-           }
-           $x1 = $input[$i][0][0];
-           $y1 = $input[$i][0][1];
-       }
-       //2,0 -> 6,4 = 2,0 3,1 4,2 5,3 6,4
-       if($x1 < $x2 && $y1 < $y2)
-       {
-           while($x1 <= $x2)
-           {
-               $markedOceanFloor[intval($x1)][intval($y1)] += 1;
-               $x1++;
-               $y1++;
-           }
-           $x1 = $input[$i][0][0];
-           $y1 = $input[$i][0][1];
-       }
-
-       //echo intval($x1) . " " . intval($y1) . "<br>";
-       //echo intval($x2) . " " . intval($y2) . "<br>";
-       // $markedOceanFloor[intval($x1)][intval($y1)] += 1;
-       // $markedOceanFloor[intval($x2)][intval($y2)] += 1;
    }
     return $markedOceanFloor;
 }
 
-$markedOceanFloor = markFloor($setOceanFloor, $setInput);
+function markFloorDiagonal($oceanFloor, $input)
+{
+    $markedOceanFloor = $oceanFloor;
+    //echo count($input); exit;
+    for($i = 0; $i < count($input); $i++) {
+        $x1 = $input[$i][0][0];
+        $y1 = $input[$i][0][1];
 
-/*echo "<pre>";
+        $x2 = $input[$i][1][0];
+        $y2 = $input[$i][1][1];
+
+        // 4,3 -> 2,1 = 4,1 3,2 2,3
+        // 4,1 -> 2,3 =
+        // 2,3 -> 4,1 = 2,3 3,2 4,1
+        //if(($x1 - $x2) == ($))
+
+        //
+    }
+    return $markedOceanFloor;
+}
+
+$markedOceanFloor = markFloorStraight($setOceanFloor, $setInput);
+$markedOceanFloor2 = markFloorDiagonal($markedOceanFloor, $setInput);
+
+
+echo "<pre>";
 print_r($markedOceanFloor);
-echo "</pre>";*/
+echo "</pre>";
 
 $total = 0;
 
