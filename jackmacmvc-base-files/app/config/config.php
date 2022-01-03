68fd5144c3dab640045fc3b2fe1ea1486291d39b
Jack McLoughlin <jack.mcloughlin@alig2124.staff.aligent.consulting>
2022-01-03 11:14:19 +1030

    addind mvc folders2

---
+++ jackmacmvc-base-files/app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+    // DB Params
+    const DB_HOST = 'db';
+    const DB_USER = 'YOUR_USERNAME';
+    const DB_PASS = 'YOUR_PASS';
+    const DB_NAME = 'YOUR_DBNAME';
+
+    // App Root
+    define('APPROOT', dirname(__FILE__, 2));
+    // URL Root
+    const URLROOT = 'YOUR_URL';
+    // Site Name
+    const SITENAME = 'YOUR_SITENAME';
